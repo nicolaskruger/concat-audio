747e2a49c229a1c2b998c414f154d1c40c927a56
nkruger <nicolas.nkruger@gmail.com>
2023-06-20 15:40:43 -0300

    feat: :sparkles: concat audios

--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
-console.log("hello word");
+import fs from "fs";
+import audioconcat from "audioconcat";
+import { path } from "@ffmpeg-installer/ffmpeg";
+import ffmpeg from "fluent-ffmpeg";
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+ffmpeg.setFfmpegPath(path);
+
+const extensions = ".ogg";
+const audios = ["01", "02", "03", "04"];
+
+const run = async () => {
+  audios.forEach((audio) => {
+    ffmpeg(`./audios/${audio}${extensions}`)
+      .toFormat("mp3")
+      .on("error", (err) => console.error("An error occurred: ", err))
+      .on("end", () => console.info("Audio prompts done"))
+      .save(`${audio}.mp3`);
+  });
+
+  await delay(250 * audios.length);
+
+  audioconcat(audios.map((a) => `${a}.mp3`))
+    .concat("./audios/all.mp3")
+    .on("start", function (command) {
+      console.log("ffmpeg process started:", command);
+    })
+    .on("error", function (err, stdout, stderr) {
+      console.error("Error:", err);
+      console.error("ffmpeg stderr:", stderr);
+    })
+    .on("end", function (output) {
+      console.error("Audio created in:", output);
+    });
+  await delay(250 * audios.length);
+  audios.forEach((audio) => {
+    fs.unlink(`${audio}.mp3`, () => {});
+  });
+};
+
+run();
---
+++ readme.md
@@ -0,0 +1,17 @@
+# concatenate audio
+
+## usage
+
+- install de dependence `yarn`
+- paste your audio file in the `./audios` folder.
+- replace `extensions` with your extension
+- replace `audios` with the name of your files without the extension
+
+```typescript
+// files a.mp3, b.mp3, c.mp3
+
+const audios = ["a","b","c"]
+
+```
+
+- run the project `yarn run dev`
